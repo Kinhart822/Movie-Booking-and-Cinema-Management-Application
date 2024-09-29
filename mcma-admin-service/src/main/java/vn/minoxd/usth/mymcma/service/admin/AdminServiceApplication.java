aee400229d7f277156709cf8205b8e2bdc4b4893
minoxd <minophen.aceta114@gmail.com>
2024-09-29 14:30:33 +0700

    build: SpringBootApplication init

---
+++ mcma-admin-service/src/main/java/vn/minoxd/usth/mymcma/service/admin/AdminServiceApplication.java
@@ -0,0 +1,7 @@
+package vn.minoxd.usth.mymcma.service.admin;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminServiceApplication {
+}
