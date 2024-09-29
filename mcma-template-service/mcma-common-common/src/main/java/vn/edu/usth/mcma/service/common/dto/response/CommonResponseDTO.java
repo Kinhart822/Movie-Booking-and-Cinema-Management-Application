c4f99cdec3ce0cbfc80061428542767fe83b91be
minoxd <minophen.aceta114@gmail.com>
2024-09-29 17:12:53 +0700

    feat: common response dto

---
+++ mcma-template-service/mcma-common-common/src/main/java/vn/edu/usth/mcma/service/common/dto/response/CommonResponseDTO.java
@@ -0,0 +1,18 @@
+package vn.edu.usth.mcma.service.common.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommonResponseDTO {
+
+    private String response;
+    private String status;
+}
