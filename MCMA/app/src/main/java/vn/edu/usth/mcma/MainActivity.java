465afcd44ccf265a752f5f3775b4c3ef7857ab11
Vietanh145 <nguyenvietanh9955@gmail.com>
2024-10-14 08:42:13 +0700

    Update menu button when clicking it will show a list (maybe)

--- MCMA/app/src/main/java/vn/edu/usth/mcma/MainActivity.java
+++ MCMA/app/src/main/java/vn/edu/usth/mcma/MainActivity.java
@@ -1,24 +1,38 @@
 package vn.edu.usth.mcma;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager2 mviewPager;
+
+    private BottomNavigationView bottomNavigationView;
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
     }
 }
---
+++ MCMA/app/src/main/java/vn/edu/usth/mcma/frontend/ListFragment.java
@@ -0,0 +1,44 @@
+package vn.edu.usth.mcma.frontend;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import vn.edu.usth.mcma.R;
+
+
+public class ListFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_list
+                , container, false);
+
+        ImageButton mImageView = v.findViewById(R.id.menu_button);
+
+        DrawerLayout mDrawerLayout = v.findViewById(R.id.list_fragment);
+
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mDrawerLayout != null && !mDrawerLayout.isDrawerOpen(GravityCompat.END)) {
+                    mDrawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+
+        return v;
+    }
+}
