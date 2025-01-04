61a3c822ee88fbaa80082e9bfe61093fab4d6398
minoxd <minophen.aceta114@gmail.com>
2025-01-04 07:01:02 +0700

    disable entity listener

--- mcma-backend/src/main/java/vn/edu/usth/mcma/backend/entity/AbstractAuditing.java
+++ mcma-backend/src/main/java/vn/edu/usth/mcma/backend/entity/AbstractAuditing.java
@@ -1,7 +1,6 @@
 package vn.edu.usth.mcma.backend.entity;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -9,7 +8,6 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -20,7 +18,7 @@ import java.time.Instant;
  *      created by, last modified by, created date, last modified date
  */
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
+//@EntityListeners(AuditingEntityListener.class) error: loop
 @Getter
 @Setter
 public class AbstractAuditing implements Serializable {
