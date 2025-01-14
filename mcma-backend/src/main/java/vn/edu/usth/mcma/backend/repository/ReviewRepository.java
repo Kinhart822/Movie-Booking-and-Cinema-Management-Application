caf34fbb765847dce9663eb66dae485efea42b81
minoxd <minophen.aceta114@gmail.com>
2025-01-14 16:47:41 +0700

    refactor code

--- mcma-backend/src/main/java/vn/edu/usth/mcma/backend/repository/ReviewRepository.java
+++ mcma-backend/src/main/java/vn/edu/usth/mcma/backend/repository/ReviewRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import vn.edu.usth.mcma.backend.dto.HighRatingMovieProjection;
-import vn.edu.usth.mcma.backend.dto.ReviewPresentation;
 import vn.edu.usth.mcma.backend.entity.Movie;
 import vn.edu.usth.mcma.backend.entity.Review;
 
--- mcma-backend/src/main/java/vn/edu/usth/mcma/backend/repository/ScheduleRepository.java
+++ mcma-backend/src/main/java/vn/edu/usth/mcma/backend/repository/ScheduleRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import vn.edu.usth.mcma.backend.dto.SchedulePresentation;
 import vn.edu.usth.mcma.backend.entity.Movie;
 import vn.edu.usth.mcma.backend.entity.Schedule;
 import vn.edu.usth.mcma.backend.entity.Screen;
