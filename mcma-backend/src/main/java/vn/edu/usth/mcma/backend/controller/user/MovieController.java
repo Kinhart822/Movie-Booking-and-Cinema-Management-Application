4e1949a3921c9d7d8cf2098da2ef552b85bb76fa
minoxd <minophen.aceta114@gmail.com>
2025-01-22 18:05:36 +0700

    refactor code

--- mcma-backend/src/main/java/vn/edu/usth/mcma/backend/controller/user/MovieController.java
+++ mcma-backend/src/main/java/vn/edu/usth/mcma/backend/controller/user/MovieController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
-public class HomeController {
+public class MovieController {
     private final MovieService movieService;
 
     @GetMapping("/movie/now-showing")
